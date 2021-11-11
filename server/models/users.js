274d33104fe5a671666c42593c2d13a0ad204d8a
ryannarnoldd <ryannarnoldd@gmail.com>
2021-11-11 10:50:02 -0500

    Update to-date changes, white screen soon to fix.

--- server/models/users.js
+++ server/models/users.js
@@ -1,4 +1,8 @@
 const bcrypt = require('bcrypt');
+const { result } = require('lodash')
+const { client } = require('./mongo');
+
+const collection = client.db(process.env.MONGO_DB).collection("users");
 
 const list = [
     { 
@@ -41,22 +45,26 @@ const list = [
 
 ];
 
-module.exports.GetAll = function GetAll() { return list; }
-module.exports.Get = user_id => list[user_id];
+module.exports.GetAll = function GetAll() { return collection.find().toArray(); };
+
+module.exports.Get = user_id => collection.findOne({ _id: user_id });
+
 module.exports.GetByHandle = function GetByHandle(handle) { return ({ ...list.find( x => x.handle == handle ), password: undefined }); } 
-module.exports.Add = function Add(user, cb) {
+
+
+module.exports.Add = async function Add(user) {
     if(!user.firstName){
         return Promise.reject( { code: 422, msg: "First Name is required" } )
     }
 
-    bcrypt.hash(user.pasword, +process.env.SALT_ROUNDS)
-        .then(hash=> {
-            user.password = hash;
+    const hash = await bcrypt.hash(user.pasword, +process.env.SALT_ROUNDS)
+    
+    user.password = hash;
 
-            list.push(user);
+    const user2 = await collection.insertOne(user);
+    user._id = user2.insertedId;
 
-            return { ...user, password: undefined };
-        })
+    return { ...user2.ops[0], password: undefined };
 };
 
 
@@ -84,22 +92,26 @@ module.exports.Delete = function Delete(user_id) {
     return user;
 }
 
-module.exports.Login = function Login(handle, password){
+module.exports.Login = async function Login(handle, password){
     console.log(handle, password);
-    const user = list.find(x=> x.handle == handle);
-    if(!user) {
+    const user = collection.findOne({ handle });
+
+    if (!user) {
         return Promise.reject( { code: 401, msg: "User not found" } );
     }
 
+    const result = await bcrypt.compare(password, user.password)
 
-    return bcrypt.compare(password, user.password).then(result => {
-        if(!result) {
-            throw { code: 401, msg: "Wrong password" };
-        }
+    if (!result) {
+        throw { code: 401, msg: "Wrong password" };
+    }
+        
+    const data = { ...user, password: undefined };
     
-        const data = { ...user, password: undefined };
-    
-        cb(null, {user: data});
-    });
+    return { user: data}
 
+}
+
+module.exports.seed = () => {
+    list.forEach(x => module.exports.Add(x));
 }
